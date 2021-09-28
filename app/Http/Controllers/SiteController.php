15c26802cc873af0dfe545f4e9dae2a78beb27e9
vitaliy-ce <ce.vitaliy@ya.ru>
2021-09-28 23:11:39 +0500

    Реалищована регистрация/авторизация

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -8,6 +8,16 @@ use App\Models\Site;
 class SiteController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
